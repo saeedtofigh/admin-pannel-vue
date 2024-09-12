2b247647098fd353d12bda0b83cb85baa1540ea6
Zarpoosh <mzar67839@gmail.com>
2024-09-12 22:23:42 +0330

    commit all

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
+  <HomeView/>
   <!-- <Navbar /> -->
   <RouterView />
 </template>
 
 <script>
+import HomeView from "./views/HomeView.vue";
 import { RouterLink, RouterView } from "vue-router";
 // import Navbar from "./components/Navbar.vue";
 export default {
-  // components: { Navbar },
+  components: { HomeView},
 };
 </script>
 
